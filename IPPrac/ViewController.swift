112ac43bbb5426d3947ddc5408dc95f7492197be
Charles Cheng <cowton0517@gmail.com>
2022-12-16 10:19:19 +0800

    Initial Commit

---
+++ IPPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPPrac
+//
+//  Created by Chun-Li Cheng on 2022/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
